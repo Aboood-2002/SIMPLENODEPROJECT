093a674d1030f5070278dcaac6ae005d6ebdc271
aboood-2002 <aboodahm807@gamil.com>
2024-02-27 16:25:40 +0200

    the rest of project include html,css,node modules & images

---
+++ productmodel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+    {
+        name : String,
+        price : Number,
+        image : String,
+    }
+)
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
